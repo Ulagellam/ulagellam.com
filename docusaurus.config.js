6c2b8072c2468350df40159fdf00d3658649f633
Sivayogeith <sivayogeith@gmail.com>
2023-03-02 20:08:55 +0530

    trying to deploy the website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ulagellam',
-  tagline: 'Products beyond boundaries!',
-  url: 'https://github.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-
+  title: "Ulagellam",
+  tagline: "Products beyond boundaries!",
+  url: "https://ulagellam.github.io/ulagellam-website",
+  baseUrl: "/ulagellam-website/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ulagellam', // Usually your GitHub org/user name.
-  projectName: 'ulagellam-website', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "Ulagellam", // Usually your GitHub org/user name.
+  projectName: "ulagellam-website", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,77 +57,76 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Ulagellam',
+        title: "Ulagellam",
         logo: {
-          alt: 'Ulagellam Logo',
-          src: 'img/favicon.png',
+          alt: "Ulagellam Logo",
+          src: "img/favicon.png",
         },
         items: [
           {
-            to: '/products',
-            position: 'right',
-            label: 'Products',
+            to: "/products",
+            position: "right",
+            label: "Products",
           },
           {
-            to: '/services',
-            position: 'right',
-            label: 'Services',
+            to: "/services",
+            position: "right",
+            label: "Services",
           },
           {
-            to: '/training',
-            position: 'right',
-            label: 'Training',
+            to: "/training",
+            position: "right",
+            label: "Training",
           },
           {
-            to: '/research',
-            position: 'right',
-            label: 'R&D',
+            to: "/research",
+            position: "right",
+            label: "R&D",
           },
-          {to: '/blog', label: 'Blog', position: 'right'},
+          { to: "/blog", label: "Blog", position: "right" },
         ],
       },
       footer: {
-
         links: [
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Facebook',
-                href: 'https://www.facebook.com/ulagellam',
+                label: "Facebook",
+                href: "https://www.facebook.com/ulagellam",
               },
-  
+
               {
-                label: 'Linkedin',
-                href: 'https://www.linkedin.com/in/umamaheswarantg',
+                label: "Linkedin",
+                href: "https://www.linkedin.com/in/umamaheswarantg",
               },
               {
-                label: 'Youtube',
-                href: 'https://www.youtube.com/channel/UClind4orH7yfe0Qp9bzm6ug',
+                label: "Youtube",
+                href: "https://www.youtube.com/channel/UClind4orH7yfe0Qp9bzm6ug",
               },
             ],
           },
           {
-            title: 'Quick Links',
+            title: "Quick Links",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'About Us',
-                to: '/about-us',
+                label: "About Us",
+                to: "/about-us",
               },
             ],
           },
         ],
-          logo: {
-            alt: 'Ulagellam Logo',
-            src: 'img/ulagellam-logo.png',
-            srcDark: "img/ulagellam-logo-dark.png",
-            width: 170,
-            height: 51,
-          },
+        logo: {
+          alt: "Ulagellam Logo",
+          src: "img/ulagellam-logo.png",
+          srcDark: "img/ulagellam-logo-dark.png",
+          width: 170,
+          height: 51,
+        },
         copyright: `© 2016-${new Date().getFullYear()} Ulagellam Technologies Pvt Ltd. All Rights Reserved.`,
       },
       prism: {
